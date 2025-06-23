d96bd3857211e5dd57bf01e489e51c0e23660996
juicyacer <dmmy1907@gmail.com>
2025-06-23 13:11:01 -0600

    acp

---
+++ lesson #1/main.py
@@ -0,0 +1,11 @@
+print("hi")
+UserName = "juicyacer"
+age = 14
+isStudent = True
+
+print("Welcome", UserName)
+favGames = "Halo , battle front 2 2019"
+print("Myfavgames",favGames)
+git config --global user.name "juicyacer"
+
+git config --global user.email "dmmy1907@gmail.com"
