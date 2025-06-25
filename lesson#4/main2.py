5fc1bc5f7d639ddcf87705b579ea75e4ecc006d2
juicyacer <dmmy1907@gmail.com>
2025-06-25 13:54:39 -0600

    acp5

--- lesson#4/main2.py
+++ lesson#4/main2.py
@@ -8,3 +8,16 @@ sum = math + english + science + computerscience
 perc = (sum/400)*100
 
 print(f"you gat {sum}/400 with {perc} %")
+
+
+print("Enter Marks Obtained in 4 subjects out of 100: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+adventureED = int(input("adventureED :"))
+pe = int(input("pe :"))
+
+sum = math + english + science + adventureED
+perc = (sum/400)*100
+
+print(f"you gat {sum}/400 with {perc} %")
