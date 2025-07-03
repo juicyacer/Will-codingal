c680e3d1751f6d7af9054d4bc000f048eed99257
juicyacer <dmmy1907@gmail.com>
2025-07-03 13:43:38 -0600

    acp

---
+++ lesson#8/main.py
@@ -0,0 +1,61 @@
+for i in range(1, 11):
+    print(f"number: {i}")
+
+friends = ["evan", "hudson", "iziah"]
+for i in friends:
+    print(i)
+
+name = "python"
+for i in name:
+    print(i)
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print("* ", end=" ")
+    print()
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(f"{i * j}", end="\t")
+    print()
+
+for i in range(1, 6):
+    for j in range(1 , i+1):
+        print('* ',end='')
+    print()
+
+number = 2
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * I}")
+
+for i in range(1, 6):
+    print('* ' *i)
+
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print("* ", end=" ")
+    print()
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(f"{i * j}", end="\t")
+    print()
+
+for i in range(1, 6):
+    for j in range(1 , i+1):
+        print('* ',end='')
+    print()
+
+number = 2
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * I}")
+
+for i in range(1, 6):
+    print('* ' *i)
---
+++ lesson#9/main.py
@@ -0,0 +1,30 @@
+i = 1
+while(i <= 5):
+    print("value of i:", i)
+    i = i + 1
+
+n = int(input("enter the value of terms: "))
+
+sum = 0
+
+i = 1
+while i <= n:
+    sum = sum + ii = i + 1
+
+print("\nSum =", sum)
+
+
+i = 1
+while(i <= 5):
+    print("value of i:", i)
+    i = i + 1
+
+n = int(input("enter the value of terms: "))
+
+sum = 0
+
+i = 1
+while i <= n:
+    sum = sum + ii = i + 1
+
+print("\nSum =", sum)
